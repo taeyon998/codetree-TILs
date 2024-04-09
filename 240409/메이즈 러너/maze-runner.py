1df1b3104c84c501c7466f91f9ef7a4dc06d4541
taeyon998 <65479405+taeyon998@users.noreply.github.com>
2024-04-09 18:13:27 +0900

    Title: 메이즈 러너, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240409/메이즈 러너/maze-runner.py
+++ 240409/메이즈 러너/maze-runner.py
@@ -1,1 +1,34 @@
-jn
+# 16m (/1hr)
+class Player:
+    def __init__(self,r,c):
+        self.r=r
+        self.c=c
+        self.escape=False
+        self.d=0 
+    def printp(self):
+        print(f'r={self.r},c={self.c},esc={self.escape},d={self.d}')
+
+def dist(r1,c1,r2,c2):
+    return math.abs(r1-c1)2+math.abs(r2-c2)
+
+
+# setup
+N,M,K = list(map(int,input().split()))
+board=[]
+for _ in range(N):
+    ln=list(map(int,input().split()))
+    board.append(ln)
+plist=[]
+for _ in range(M):
+    r,c=list(map(int,input().split()))
+    plist.append(Player(r-1,c-1))
+e=list(map(int,input().split()))
+
+# 1) player move
+for p in plist:
+    if p.escape:
+        continue
+    dirs = [(-1,0),(1,0),(0,-1),(0,1)]
+    minD=dist(p.r,p.c,e[0],e[1])
+    for dr,dc in dirs:
+        nR,nC = p.r+dr,p.c+dc
